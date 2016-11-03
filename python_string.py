604777056172ab409e8b61767bad60d9e139cdfa
nbaruah <nayanmanibaruah@gmail.com>
2016-11-03 13:16:48 +0530

    python string literals

---
+++ python_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+""" Python strings
+----------------------------
+Use built-in string class named "str"
+'String "literal"'
+"String 'literal'"
+"""
+
+string_literal_1 = 'single quotes can contain "double quotes"'
+string_literal_2 = "double quotes can contain 'single quotes'"
+string_escape = "we can escape \" special characters by \\"
+
+print string_literal_1
+print string_literal_2
+print string_escape
